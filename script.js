e6f45182dbc171ff0cadc14b3757cbc03dd5da17
di-wee <Dionis.w96@gmail.com>
2023-07-06 18:02:10 +0800

    genGrid  completed

---
+++ script.js
@@ -0,0 +1,26 @@
+let container = document.querySelector('.container');
+let candies = ['Blue', 'Green', 'Orange', 'Purple', 'Red', 'Yellow'];
+
+//game logic:
+
+function gameInit() {
+	genGrid();
+}
+
+function genGrid() {
+	for (let i = 1; i < 7; i++) {
+		for (let j = 1; j < 7; j++) {
+			const div = document.createElement('div');
+			div.className = 'candies';
+			div.setAttribute('style', `grid-area:${i}/${j}/${i}/${j}`);
+			const index = Math.floor(Math.random() * candies.length);
+			const chosenCandy = candies[index];
+			const imgEl = document.createElement('img');
+			imgEl.setAttribute('src', `/candies/${chosenCandy}.png`);
+			div.appendChild(imgEl);
+			container.appendChild(div);
+		}
+	}
+}
+
+genGrid();
