133266188aed850a8d4e0adba0613190a0556329
di-wee <callmehdion@gmail.com>
2023-07-07 17:03:27 +0800

    updated candy images, added logo

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function genGrid() {
 	for (let i = 1; i < 7; i++) {
 		for (let j = 1; j < 7; j++) {
 			const div = document.createElement('div');
-			div.className = 'candies';
+			div.className = `${i}-${j}`;
 			div.setAttribute('style', `grid-area:${i}/${j}/${i}/${j}`);
 			const index = Math.floor(Math.random() * candies.length);
 			const chosenCandy = candies[index];
 			const imgEl = document.createElement('img');
-			imgEl.setAttribute('src', `/candies/${chosenCandy}.png`);
+			imgEl.src = `/candies/${chosenCandy}.png`;
 			div.appendChild(imgEl);
 			container.appendChild(div);
 		}
