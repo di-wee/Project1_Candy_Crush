7ef0e89a24bc76d5b3c1f5ed1924046fd0abdab7
di-wee <callmehdion@gmail.com>
2023-07-10 11:06:23 +0800

    validMove edited (works at last)

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ let totalRows = 5;
 let totalColumn = 5;
 let grid = [];
 
-//game logic:
-
 function gameInit() {
 	genGrid();
 	eventListener();
@@ -28,7 +26,6 @@ function genGrid() {
 			const chosenCandy = candies[index];
 			const imgEl = document.createElement('img');
 			imgEl.src = `/candies/${chosenCandy}.png`;
-			imgEl.id = 'lolli';
 			imgEl.className = `${i}-${j}`;
 			div.appendChild(imgEl);
 			container.appendChild(div);
@@ -36,7 +33,7 @@ function genGrid() {
 		}
 		grid.push(rows);
 	}
-	console.log(grid);
+	console.log(grid); //candies element now pushed to into an array to be accessed directly
 }
 
 //event listeners
@@ -69,13 +66,19 @@ function eventListener() {
 	}
 
 	function dragEnd() {
-		//only allow adjacent candies
 		const pickedArr = pickedCandy.className.split('-'); //return ['index', '1', '1']
 		const pickedRow = Number(pickedArr[0]); // convert string into number
 		const pickedColumn = Number(pickedArr[1]);
 		const swopArr = swoppedCandy.className.split('-');
 		const swoppedRow = Number(swopArr[0]);
 		const swoppedColumn = Number(swopArr[1]);
+
+		function swopCandies() {
+			let pickedImg = pickedCandy.src;
+			let swoppedImg = swoppedCandy.src;
+			pickedCandy.src = swoppedImg;
+			swoppedCandy.src = pickedImg;
+		}
 
 		let swopRight =
 			pickedRow === swoppedRow && swoppedColumn === pickedColumn + 1; //if picked is [1/2] swopped is [1/3]
@@ -85,104 +88,97 @@ function eventListener() {
 			swoppedRow === pickedRow - 1 && swoppedColumn === pickedColumn; //if picked is [2/1], swop is  [1/1]
 		let swopBottom =
 			swoppedRow === pickedRow + 1 && swoppedColumn === pickedColumn; //if picked is [2/1], swop is [3/1]
+
 		const checkAdjacent = swopRight || swopLeft || swopTop || swopBottom;
+
 		if (checkAdjacent) {
-			//swopping candies mechanic
-			function swopCandies() {
-				let pickedImg = pickedCandy.src;
-				let swoppedImg = swoppedCandy.src;
-				pickedCandy.src = swoppedImg;
-				swoppedCandy.src = pickedImg;
-			}
 			swopCandies();
-			const letValid = checkValidMove(); //if not nothing to crush then swop back candies
-			if (!letValid) {
-				swopCandies();
+		}
+
+		let checkValid = isValid();
+
+		if (!checkValid) {
+			swopCandies();
+		}
+
+		crushCandies();
+
+		function crushCandies() {
+			crush3Candies();
+			// cascadeCandies();
+			// crush4Candies();
+			// crush5Candies();
+
+			function crush3Candies() {
+				//horizontalcrush
+				for (let r = 0; r < totalRows; r++) {
+					for (let c = 0; c < totalColumn - 2; c++) {
+						let candy1 = grid[r][c];
+						let candy2 = grid[r][c + 1];
+						let candy3 = grid[r][c + 2];
+						if (
+							candy1.src == candy2.src &&
+							candy2.src == candy3.src &&
+							!(candy1.src = '')
+						) {
+							candy1.src = '';
+							candy2.src = '';
+							candy3.src = '';
+						}
+					}
+				}
+
+				//verticalcrush
+				for (let r = 0; r < totalRows - 2; r++) {
+					for (let c = 0; c < totalColumn; c++) {
+						let candy1 = grid[r][c];
+						let candy2 = grid[r + 1][c];
+						let candy3 = grid[r + 2][c];
+						if (
+							candy1.src == candy2.src &&
+							candy2.src == candy3.src &&
+							!(candy1.src = '')
+						) {
+							//if candies get crushed .src = blank
+							candy1.src = '';
+							candy2.src = '';
+							candy3.src = '';
+						}
+					}
+				}
 			}
 		}
 
-		function checkValidMove() {
+		function isValid() {
 			for (let r = 0; r < totalRows; r++) {
 				for (let c = 0; c < totalColumn - 2; c++) {
 					let candy1 = grid[r][c];
 					let candy2 = grid[r][c + 1];
 					let candy3 = grid[r][c + 2];
-					if (candy1.src == candy2.src && candy2.src == candy3.src) {
+					if (
+						candy1.src == candy2.src &&
+						candy2.src == candy3.src &&
+						!(candy1.src = '')
+					) {
 						return true;
 					}
 				}
 			}
-
 			for (let r = 0; r < totalRows - 2; r++) {
 				for (let c = 0; c < totalColumn; c++) {
 					let candy1 = grid[r][c];
 					let candy2 = grid[r + 1][c];
 					let candy3 = grid[r + 2][c];
-					if (candy1.src == candy2.src && candy2.src == candy3.src) {
+					if (
+						candy1.src == candy2.src &&
+						candy2.src == candy3.src &&
+						!(candy1.src = '')
+					) {
 						return true;
 					}
 				}
 			}
 			return false;
-		}
-		//crushing candies mechanic
-		crushCandies();
-		function crushCandies() {
-			crush3Candies();
-			cascadeCandies();
-			// crush4Candies();
-			// crush5Candies();
-
-			function crush3Candies() {
-				horizontalCrush();
-				verticalCrush();
-				//horizontal
-				function horizontalCrush() {
-					for (let r = 0; r < totalRows; r++) {
-						for (let c = 0; c < totalColumn - 2; c++) {
-							let candy1 = grid[r][c];
-							let candy2 = grid[r][c + 1];
-							let candy3 = grid[r][c + 2];
-							if (candy1.src == candy2.src && candy2.src == candy3.src) {
-								candy1.src = '';
-								candy2.src = '';
-								candy3.src = '';
-							}
-						}
-					}
-				}
-
-				//vertical
-				function verticalCrush() {
-					for (let r = 0; r < totalRows - 2; r++) {
-						for (let c = 0; c < totalColumn; c++) {
-							let candy1 = grid[r][c];
-							let candy2 = grid[r + 1][c];
-							let candy3 = grid[r + 2][c];
-							if (candy1.src == candy2.src && candy2.src == candy3.src) {
-								candy1.src = '';
-								candy2.src = '';
-								candy3.src = '';
-							}
-						}
-					}
-				}
-			}
-
-			function cascadeCandies() {
-				for (let r = totalRows - 1; r >= 0; r--) {
-					let rowInd = totalRows - 2; //3
-					for (let c = 0; c < totalColumn; c++) {
-						let candy = grid[r][c]; //4-1
-						let topCandy = grid[rowInd][c]; // 3-1
-						if (!candy.src && !topCandy.src) {
-							rowInd -= 1;
-						} else if (!candy.src && topCandy.src) {
-							topCandy.src = candy.src;
-						}
-					}
-				}
-			}
 		}
 	}
 }
